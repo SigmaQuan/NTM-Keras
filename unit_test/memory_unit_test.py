e42d34e2e3163488daff15c5b584d5f3757d162f
quanzhbin <quanzhibin@gmail.com>
2016-12-09 20:52:19 +0800

    Update code of NTM based on Keras.

--- unit_test/memory_unit_test.py
+++ unit_test/memory_unit_test.py
@@ -1,19 +1,31 @@
+from keras import backend as K
+import theano.tensor as T
+import theano
 import memory
 import head
-# import write_heads
+#
+# number_of_memory_locations = 6
+# memory_vector_size = 3
+#
+# memory_t = memory.initial(number_of_memory_locations, memory_vector_size)
+#
+# weight_t = K.random_binomial((number_of_memory_locations, 1), 0.2)
+#
+# read_vector = head.reading(memory_t, weight_t)
+#
+# print memory_t.shape
+# print weight_t.shape
+# print read_vector
+#
 
-from keras import backend as K
 
-number_of_memory_locations = 6
-memory_vector_size = 3
+def logistic(x):
+    s = 1 / (1 + K.exp(x))
+    log = theano.function([x], s)
+    return log
+    # return s
 
-memory_t = memory.initial(number_of_memory_locations, memory_vector_size)
+x = [[0, 1], [-1, -2]]
+print logistic(x)
 
-weight_t = K.random_binomial((number_of_memory_locations, 1), 0.2)
-
-read_vector = head.reading(memory_t, weight_t)
-
-print memory_t.shape
-print weight_t.shape
-print read_vector
 
