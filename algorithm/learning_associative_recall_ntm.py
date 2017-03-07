df335d7336fbc46cb3cf0c6eda6c1a5fa044f2d7
robot steven <quan.zhibin@gmail.com>
2017-03-07 19:17:51 +0800

    New a Python file for learning associative recall algorithm with NTM.

---
+++ algorithm/learning_associative_recall_ntm.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""An implementation of learning associative recall algorithm with NTM.
+Input sequence length: "2 ~ 6 items: (2*(3+1) ~ 6*(3+1))."
+Input dimension: "6+2", Item 3*6 bits
+Output sequence length: "3" one item .
+Output dimension: equal to input dimension.
+"""
+
