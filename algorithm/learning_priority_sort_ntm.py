5c650eb599e621e668a433b90290b96c221d6ecc
robot steven <quan.zhibin@gmail.com>
2017-03-07 19:21:07 +0800

    New a Python file for learning priority sort algorithm with NTM.

---
+++ algorithm/learning_priority_sort_ntm.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""An implementation of learning priority sort algorithm with NTM.
+Input sequence length: "1 ~ 20: (1*2+1)=3 ~ (20*2+1)=41"
+Input dimension: "8"
+Output sequence length: equal to input sequence length.
+Output dimension: equal to input dimension.
+"""
+
