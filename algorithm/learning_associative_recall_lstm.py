43a1652834667ee8c44dc558f93f657f2dc55225
robot steven <quan.zhibin@gmail.com>
2017-03-07 19:16:23 +0800

    Add docstring for learning associative recall algorithm with LSTM.

--- algorithm/learning_associative_recall_lstm.py
+++ algorithm/learning_associative_recall_lstm.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-'''An implementation of learning associative recall algorithm with LSTM.
+"""An implementation of learning associative recall algorithm with LSTM.
 Input sequence length: "2 ~ 6 items: (2*(3+1) ~ 6*(3+1))."
 Input dimension: "6+2", Item 3*6 bits
 Output sequence length: "3" one item .
 Output dimension: equal to input dimension.
-'''
+"""
 
 from __future__ import print_function
 from keras.models import Sequential
