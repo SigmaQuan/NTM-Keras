d03cf58852733f705703de6ac534e0e6362b8882
robot steven <quan.zhibin@gmail.com>
2017-05-31 20:21:16 +0800

    NTM for question answering on the bAbI data set.

--- main.py
+++ main.py
@@ -75,41 +75,62 @@ import struct
 #
 #
 
+#
+# id_i = 0
+# value_i = 0.0
+# id_i_1 = 0
+# value_i_1 = 1.0
+# number = 0
+# with open("copy.dat", "r") as f:
+#     for line in f:
+#         line = line.replace('(', '')
+#         line = line.replace(')', '')
+#         line = line.replace(' ', '')
+#         line = line.replace('\n', '')
+#         # print line
+#         id_value = line.split(',')
+#
+#         if len(id_value) < 2:
+#             continue
+#         # print id_value
+#
+#         id = int(id_value[0])
+#         id = id*128/1024
+#         value = float(id_value[1])
+#         # print(id)
+#         # print(value)
+#
+#         if (id_i == id):
+#             value_i = value_i + value
+#             number = number + 1
+#         elif (id < 2000):
+#
+#             if (value_i/number < (value_i_1 - 0.009) and id_i < 100) or ((id_i > (id_i_1 + 20)) and id_i > 100) :
+#                 print '({0}, {1})'.format(id_i_1+1, value_i_1)
+#                 id_i_1 = id_i
+#                 value_i_1 = value_i/number
+#
+#             id_i = id
+#             value_i = value
+#             number = 1
 
-id_i = 0
-value_i = 0.0
-id_i_1 = 0
-value_i_1 = 1.0
-number = 0
-with open("copy.dat", "r") as f:
-    for line in f:
-        line = line.replace('(', '')
-        line = line.replace(')', '')
-        line = line.replace(' ', '')
-        line = line.replace('\n', '')
-        # print line
-        id_value = line.split(',')
 
-        if len(id_value) < 2:
-            continue
-        # print id_value
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-        id = int(id_value[0])
-        id = id*128/1024
-        value = float(id_value[1])
-        # print(id)
-        # print(value)
+fig = plt.figure()
+ax = Axes3D(fig)
+N = np.arange(100, 4000, 20)
+p = np.arange(0.01, 0.99, 0.02)
+N, p = np.meshgrid(N, p)
+# R = np.sqrt(N ** 2 + p ** 2)
+# D = np.sin(R)
+D = N/p
 
-        if (id_i == id):
-            value_i = value_i + value
-            number = number + 1
-        elif (id < 2000):
+ax.plot_surface(N, p, D, rstride=1, cstride=1, cmap=plt.cm.hot)
+ax.contourf(N, p, D, zdir='z', offset=-2, cmap=plt.cm.hot)
+# ax.set_zlim(-2, 2)
 
-            if (value_i/number < (value_i_1 - 0.009) and id_i < 100) or ((id_i > (id_i_1 + 20)) and id_i > 100) :
-                print '({0}, {1})'.format(id_i_1+1, value_i_1)
-                id_i_1 = id_i
-                value_i_1 = value_i/number
-
-            id_i = id
-            value_i = value
-            number = 1
+# savefig('../figures/plot3d_ex.png',dpi=48)
+plt.show()
